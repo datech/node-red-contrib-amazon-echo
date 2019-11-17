0d9a73f3527f3836415704cf769e24bf7be4811f
Stefan Kalscheuer <stefan@stklcode.de>
2019-11-17 17:10:45 +0100

    migrate set-state endpoints to model class instead of JSON templates
    Only add errors if the unsupported parameter has actually been reuested.

--- api/hue/model/Registration.js
+++ api/hue/model/Registration.js
@@ -2,16 +2,16 @@
  * Registration response.
  */
 module.exports = class Registration extends Array {
-    /**
-     * Add username to success list.
-     *
-     * @param {string} username Registered username.
-     */
-    success(username) {
-        this.push({
-            success: {
-                username: username
-            }
-        })
-    }
+  /**
+   * Add username to success list.
+   *
+   * @param {string} username Registered username.
+   */
+  success(username) {
+    this.push({
+      success: {
+        username: username
+      }
+    })
+  }
 };
